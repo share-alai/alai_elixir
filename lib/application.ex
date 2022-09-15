589721346ce7de8f4aacefff01f08a47bb8e328c
Andrea Lai <alai.arpas@gmail.com>
2022-09-15 16:30:21 +0200

    application IO.puts

--- lib/application.ex
+++ lib/application.ex
@@ -5,7 +5,8 @@ defmodule AlaiElixir.Application do
 
   @impl true
   def start(_type, _args) do
-    IO.puts("started my app")
+    IO.puts("Started my app -> AlaiElixir.Application lib/Application")
+    AlaiElixir.where()
     children = []
     opts = [strategy: :one_for_one, name: AlaiElixir.Supervisor]
     Supervisor.start_link(children, opts)
